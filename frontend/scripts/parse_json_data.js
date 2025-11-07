c70d29458e516262c01c8bf7334a64abf4c89fbd
alxww55 <alxww55@icloud.com>
2025-11-07 19:56:58 +0100

    Created MVP frontend

---
+++ frontend/scripts/parse_json_data.js
@@ -0,0 +1,16 @@
+function renderShortenedUrl(data) {
+    const container = document.getElementById('result')
+    const urlPrefix = document.getElementById('server_url').textContent
+    const shortenedCode = urlPrefix + data.shortened_code
+
+    document.getElementById('result').textContent = shortenedCode
+}
+
+document.body.addEventListener('htmx:afterRequest', (evt) => {
+    try {
+        const response = JSON.parse(evt.detail.xhr.responseText)
+        renderShortenedUrl(response)
+    } catch (e) {
+        console.error('HTMX JSON parse error:', e)
+    }
+})
