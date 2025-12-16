58d4d676271e06bd4034f474dd9df3bcc2267d5f
alxww55 <alxww55@icloud.com>
2025-12-16 16:30:02 +0000

    Refactored main.js

--- frontend/static/scripts/main.js
+++ frontend/static/scripts/main.js
@@ -25,8 +25,6 @@ function showMessagesError(detail) {
     messageContainer.style.display = 'block';
     resultContainer.style.display = 'none';
 
-    const errorSvg = '<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path>';
-
     let errorMessage = 'Unexpected error';
     if (typeof detail === 'string') {
         errorMessage = detail;
@@ -42,7 +40,12 @@ function showMessagesError(detail) {
     svg.setAttribute('class', 'w-4 h-4');
     svg.setAttribute('fill', 'currentColor');
     svg.setAttribute('viewBox', '0 0 20 20');
-    svg.innerHTML = errorSvg;
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    path.setAttribute('fill-rule', 'evenodd');
+    path.setAttribute('d', 'M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z');
+    path.setAttribute('clip-rule', 'evenodd');
+    svg.appendChild(path);
 
     const text = document.createTextNode(errorMessage);
 
